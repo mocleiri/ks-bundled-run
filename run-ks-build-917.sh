3a1a8278a62d0fe95b616de3e3807ee11e6c1170
Michael O'Cleirigh <michael.ocleirigh@utoronto.ca>
2015-02-04 14:30:49 -0500

    Create File to bootstrap an oracle container and run ks-with-rice-bundled
    This script expects to be run on a computer setup with the docker daemon.
    
    Then it will download the oracle image and start it.
    
    Next it will run the impex process against the oracle container.
    
    Next it will run the ks-with-rice-bundled application against the provisioned
    oracle container.
    
    It will run build-917 which was the final java version of kuali student.

---
+++ run-ks-build-917.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# this assumes a fresh start each time.
+# If we were to locally tag the oracle container to form a new impexed image then it could be used instead as a starting point without requiring the impex process to run (it would save time).
+
+# this will run in the background
+echo "docker run --name oracle -d -t wnameless/oracle-xe-11g"
+docker run --name oracle -d -t wnameless/oracle-xe-11g
+
+# this will run interactively which will block the script from starting
+# the application until impex is done.
+
+echo "docker run -i -t --link oracle:db kualistudent/bundled-impex:build-917"
+docker run -i -t --link oracle:db kualistudent/bundled-impex:build-917
+
+# this will start the application in the background
+#
+echo "docker run -d -t --name ks-with-rice-bundled --link oracle:db kualistudent/bundled-app:build-917"
+docker run -d -t --name ks-with-rice-bundled --link oracle:db kualistudent/bundled-app:build-917
+
+echo "View the logs using: docker logs -f ks-with-rice-bundled"
+
